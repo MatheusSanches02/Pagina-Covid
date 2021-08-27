d80858fc7e164c30d25a260fe68f0f7e65a6bd83
Matheus Sanches <rm87426@fiap.com.br>
2021-08-27 09:00:07 -0300

    Criação do menu html

---
+++ covid19/src/components/firstPage/FirstPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Menu from '../menu/Menu'
+
+export default function FirstPage() {
+
+    return (
+        <div>
+            <Menu/>
+        </div>
+    )
+}
---
+++ covid19/src/components/menu/Menu.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Menu() {
+    return (
+        <section>
+            <header>
+                <div>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Nacional</a></li>
+                            <li><a href="#">Business</a></li>
+                            <li><a href="#">Internacional</a></li>
+                            <li><a href="#">Saúde</a></li>
+                            <li><a href="#">Tecnologia</a></li>
+                            <li><a href="#">Esporte</a></li>
+                            <li><a href="#">Entretenimento</a></li>
+                            <li><a href="#">Estilo</a></li>
+                            <li><a href="#">Viagem &amp; Gastronomia</a></li>
+                            <li><a href="#">Newsletters</a></li>
+                            <li><a href="#">Podcasts</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </section>
+    )
+}
