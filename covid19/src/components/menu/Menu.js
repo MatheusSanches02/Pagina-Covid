1160f2e91bf425f7e3afb98b3885fd1464be3d97
Matheus Sanches <rm87426@fiap.com.br>
2021-08-27 22:04:12 -0300

    Estilização do menu com hover

--- covid19/src/components/menu/Menu.js
+++ covid19/src/components/menu/Menu.js
@@ -1,27 +1,26 @@
 import React from 'react'
+import './Menu.css'
 
 export default function Menu() {
     return (
-        <section>
-            <header>
-                <div>
-                    <nav>
-                        <ul>
-                            <li><a href="#">Nacional</a></li>
-                            <li><a href="#">Business</a></li>
-                            <li><a href="#">Internacional</a></li>
-                            <li><a href="#">Saúde</a></li>
-                            <li><a href="#">Tecnologia</a></li>
-                            <li><a href="#">Esporte</a></li>
-                            <li><a href="#">Entretenimento</a></li>
-                            <li><a href="#">Estilo</a></li>
-                            <li><a href="#">Viagem &amp; Gastronomia</a></li>
-                            <li><a href="#">Newsletters</a></li>
-                            <li><a href="#">Podcasts</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
+        <section className="container-menu">
+            <div>
+                <nav id="menu">
+                    <ul>
+                        <li className="active"><a href="#">Nacional</a></li>
+                        <li><a href="#">Business</a></li>
+                        <li><a href="#">Internacional</a></li>
+                        <li><a href="#">Saúde</a></li>
+                        <li><a href="#">Tecnologia</a></li>
+                        <li><a href="#">Esporte</a></li>
+                        <li><a href="#">Entretenimento</a></li>
+                        <li><a href="#">Estilo</a></li>
+                        <li><a href="#">Viagem &amp; Gastronomia</a></li>
+                        <li><a href="#">Newsletters</a></li>
+                        <li><a href="#">Podcasts</a></li>
+                    </ul>
+                </nav>
+            </div>
         </section>
     )
 }
